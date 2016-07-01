ee17a6330566be8b44d1606a78d4dc43a76d019f
Andrew Jackson <Andrew Jackson>
2016-07-01 23:15:13 +0800

    test

--- windowstest/recipes/default.rb
+++ windowstest/recipes/default.rb
@@ -1,7 +1,7 @@
 Chef::Log.info("******Creating a data directory.******")
 
 directory 'C:\data' do
-  rights :full_control, 'WORKGROUP\andrewjacksonio'
+  rights :full_control, ['Administrators','Everyone']
   inherits false
   action :create
 end
