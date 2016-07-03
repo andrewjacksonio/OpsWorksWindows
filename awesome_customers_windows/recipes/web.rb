6f0629b9da1af68b4a83cb226f43a2ea7cd94489
Andrew Jackson <Andrew Jackson>
2016-07-03 17:19:03 +0800

    DSC IIS installed check

--- awesome_customers_windows/recipes/web.rb
+++ awesome_customers_windows/recipes/web.rb
@@ -4,8 +4,19 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-powershell_script 'Install IIS' do
-  code 'Add-WindowsFeature Web-Server'
-  guard_interpreter :powershell_script
-  not_if "(Get-WindowsFeature -Name Web-Server).Installed"
+# powershell_script 'Install IIS' do
+  # code 'Add-WindowsFeature Web-Server'
+  # guard_interpreter :powershell_script
+  # not_if "(Get-WindowsFeature -Name Web-Server).Installed"
+# end
+
+# Enable the IIS role.
+dsc_script 'Web-Server' do
+  code <<-EOH
+  WindowsFeature InstallWebServer
+  {
+    Name = "Web-Server"
+    Ensure = "Present"
+  }
+  EOH
 end
